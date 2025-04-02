a7891fd205f962052500645e5f45b8df0de0fc80
Liam Ozog <liam.ozog@gmail.com>
2025-04-01 23:26:25 -0400

    add main functionality as context menu items

--- README.md
+++ README.md
@@ -1,16 +1,7 @@
-# Spicetify Creator
+# Bound Tracks
 
-Spicetify Creator is a tool to compile modern Typescript/Javascript code to Spicetify extensions and custom apps.
+Spicetify extension - auto-queue song X if song Y ever plays
 
-## Features
-- Typescript and React syntax
-- Import node packages
-- CSS/SCSS with PostCSS support
-- Extremely fast compile time with esbuild.
-- Plugins
+## Bootstrapped with Spicetify Creator
 
-## Docs
-Check out [Spicetify's docs](https://spicetify.app/docs/development/spicetify-creator/the-basics)!
-
-## Made with Spicetify Creator
-- https://github.com/spicetify/spicetify-creator
+-   https://github.com/spicetify/spicetify-creator
--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,168 @@
-async function main() {
-    while (!Spicetify?.showNotification) {
-        await new Promise((resolve) => setTimeout(resolve, 100));
+const onSongChange = (data) => {
+    const { uri } = data?.item;
+
+    const mappings = getAllMappings();
+    const songToQueueUri = mappings[uri];
+
+    if (songToQueueUri) {
+        addPlayNext([songToQueueUri]);
+    }
+};
+
+const addPlayNext = async (uris) => {
+    await Spicetify.addToQueue(uris.map((uri) => ({ uri })));
+
+    // what is before?
+    const before = (Spicetify.Queue.nextTracks || []).filter(
+        (track) => track.provider !== "context"
+    ).length;
+
+    if (before) {
+        // what is difference?
+        const difference = Spicetify.Queue.nextTracks
+            .filter((track) => track.provider !== "context")
+            .filter((_, index) => index >= before);
+
+        await Spicetify.Platform.PlayerAPI.reorderQueue(
+            difference.map((track) => track.contextTrack),
+            { before: Spicetify.Queue.nextTracks[0].contextTrack }
+        );
+    }
+};
+
+function addSongMapping(currentSongUri, addToPlayNextUri) {
+    const STORAGE_KEY = "songMappings";
+
+    const raw = Spicetify.LocalStorage.get(STORAGE_KEY);
+    let mappings = {};
+
+    try {
+        if (raw) {
+            mappings = JSON.parse(raw);
+        }
+    } catch (e) {
+        console.warn(
+            "Failed to parse songMappings from LocalStorage, starting fresh."
+        );
     }
 
-    // Show message on start.
-    Spicetify.showNotification("Hello!");
+    mappings[currentSongUri] = addToPlayNextUri;
+
+    Spicetify.LocalStorage.set(STORAGE_KEY, JSON.stringify(mappings));
+}
+
+function printSongMappings() {
+    const STORAGE_KEY = "songMappings";
+
+    const raw = Spicetify.LocalStorage.get(STORAGE_KEY);
+    console.log("raw", raw);
+    if (!raw) {
+        console.log("No song mappings found.");
+        return;
+    }
+
+    try {
+        const mappings = JSON.parse(raw);
+        console.log("Current Song Mappings:");
+        for (const [from, to] of Object.entries(mappings)) {
+            console.log(`${from} → ${to}`);
+        }
+    } catch (e) {
+        console.error("Failed to parse songMappings from LocalStorage:", e);
+    }
+}
+
+function getAllMappings() {
+    const STORAGE_KEY = "songMappings";
+
+    const raw = Spicetify.LocalStorage.get(STORAGE_KEY);
+    if (!raw) {
+        console.log("No song mappings found.");
+        return;
+    }
+
+    try {
+        const mappings = JSON.parse(raw);
+
+        return mappings;
+    } catch (e) {
+        console.error("Failed to parse songMappings from LocalStorage:", e);
+        return [];
+    }
+}
+
+function clearSongMappings() {
+    Spicetify.LocalStorage.clear();
+}
+
+const getContextMenuItem = async (uris) => {
+    const { Type } = Spicetify.URI;
+
+    if (
+        [Type.PLAYLIST, Type.PLAYLIST_V2].includes(
+            Spicetify.URI.fromString(uris[0]).type
+        )
+    )
+        uris = (
+            await Spicetify.Platform.PlaylistAPI.getContents(uris[0])
+        ).items.map((item) => item.uri);
+
+    if (uris.length === 0) return;
+    const addToPlayNextUri = uris[0];
+    const currentSongUri = Spicetify.Player?.data?.item?.uri;
+    console.log("mapping", currentSongUri, "to", addToPlayNextUri);
+    if (addToPlayNextUri == currentSongUri) return; // don't map song to itself
+
+    addSongMapping(currentSongUri, addToPlayNextUri);
+    printSongMappings();
+};
+
+const getContextMenuItemClear = () => {
+    clearSongMappings();
+};
+
+async function main() {
+    console.log("app.tsx");
+    // if (!started) console.time("autoQueueSongMap loaded successfully");
+
+    // if (!(Spicetify.Platform && Spicetify.CosmosAsync)) {
+    //     setTimeout(() => autoQueueSongMap(true), 300);
+    //     return;
+    // }
+    // while (!Spicetify?.showNotification) {
+    //   await new Promise(resolve => setTimeout(resolve, 100));
+    // }
+
+    Spicetify.Player.addEventListener("songchange", ({ data }) =>
+        onSongChange(data)
+    );
+
+    const { Type } = Spicetify.URI;
+    const shouldShowOption = (uris) =>
+        uris.every((uri) =>
+            [
+                Type.TRACK,
+                // Type.PLAYLIST,
+                // Type.PLAYLIST_V2,
+                // Type.ALBUM,
+                // Type.LOCAL,
+                // Type.EPISODE,
+            ].includes(Spicetify.URI.fromString(uri).type)
+        );
+
+    new Spicetify.ContextMenu.Item(
+        "Always play this after the current song",
+        getContextMenuItem,
+        shouldShowOption
+    ).register();
+
+    new Spicetify.ContextMenu.Item(
+        "Clear always play next Queue",
+        getContextMenuItemClear,
+        shouldShowOption
+    ).register();
+
+    // console.timeEnd("autoQueueSongMap loaded successfully");
 }
 
 export default main;
